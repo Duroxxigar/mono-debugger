5df65724ca0e6c0c4f1b461bcc003c5e3e69dfd1
Duroxxigar <young9003@gmail.com>
2019-11-10 17:33:04 -0500

    Fixed bug with saving project settings

--- addons/mono_debugger/MonoDebugger.gd
+++ addons/mono_debugger/MonoDebugger.gd
@@ -4,12 +4,14 @@ extends EditorPlugin
 var btn: CheckBox = null
 var on: bool
 
+const setting = 'mono/debugger_agent/wait_for_debugger'
+
 func _enter_tree() -> void:
-	on = ProjectSettings.get_setting('mono/debugger_agent/wait_for_debugger')
+	on = ProjectSettings.get_setting(setting)
 	btn = CheckBox.new()
 	btn.text = 'Wait For Debugger'
 	btn.pressed = on
-	btn.connect('button_down', self, '_on_button_down')
+	btn.connect('pressed', self, '_on_button_pressed')
 	add_control_to_container(CONTAINER_TOOLBAR, btn)
 
 func _exit_tree() -> void:
@@ -17,5 +19,7 @@ func _exit_tree() -> void:
 	remove_control_from_container(CONTAINER_TOOLBAR, btn)
 	btn = null
 	
-func _on_button_down() -> void:
-	ProjectSettings.set_setting('mono/debugger_agent/wait_for_debugger', !on)
+func _on_button_pressed() -> void:
+	on = !on
+	ProjectSettings.set_setting(setting, on)
+	ProjectSettings.save()
